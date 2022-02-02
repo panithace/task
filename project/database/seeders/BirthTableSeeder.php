d3c2ac8bda3f36151b0923ca560ded00212904ef
panithace <panthea_mirhomayouni@yahoo.com>
2022-02-02 20:52:10 +0330

    Create BirthTableSeeder.php

---
+++ project/database/seeders/BirthTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BirthTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        //$faker = \Faker\Factory::create();
+        //foreach(range(1,10) as $item){
+        //     DB::table('birth')->insert([
+        //      'username' => $faker->text(20),
+        //     ]);
+        //    }
+       // factory(Birthh:class::create, 10)->create();
+    }
+}
