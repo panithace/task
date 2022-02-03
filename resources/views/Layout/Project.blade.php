39fd1757064655dc4d1818e75aebefda2875f38d
panithace <panthea_mirhomayouni@yahoo.com>
2022-02-03 16:41:21 +0330

    a5

---
+++ resources/views/Layout/Project.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Blog Home - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        @include ('layout.navigation')
+        <!-- Page content-->
+        <div class="container">
+            <div class="row">
+                <!-- Blog entries-->
+                <div class="col-lg-8">
+                    @yield('content')
+                </div>
+                   
+                <!-- Side widgets-->
+                @include ('layout.side')
+            </div>
+        </div>
+        <!-- Footer-->
+       @include('Layout.footer')
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function(){
+    return \App\Models\Birthh::factory()->has(\App\Models\Birthh::factory())->make();
+    // return \App\Models\User::fac tory()->changename('panithace')->make();
+});
+//Route::get('/', function(){
+//    birth:Birthh::orderBy('id', 'desc')->get();
+//    return view ('index');
+//});
+//Route::get('/', function(){
+//    factory(Birthh::class, 10)->create();
+//});
+//Route::get('/', function () {
+//    $birth = Birthh::all();
+//    dd($birth);
+//    return view ('index');
+//});
+
+Route::get('/about', function() {
+        return view ('about');
+});
+ 
+Route::prefix('admin')->group(function(){
+    Route::get('article/create', function(){
+        if($_GET) {
+            dd($_GET);
+        }
+         return view('admin.article.create');
+    });
+    Route::post('/article/create',function(){
+        dd('test');
+    });
+});
+//Route::get('/DB', function() {
+//$birth= DB::table('birth')->get();
+//$birth = Birthh::all();
+//dd($birth);
+//return view ('index');
+//});
+//Route::get('/DB', function() {
+ //   $users= DB::table('users')->orderBy('id')->get();
+//    dd($users);
+//    return view ('index');
+//    });
+//Route::get('/post/{p}/{id}', function ($p,$id) {
+//    return $p.'-'. $id;
+//});
+
+//Route::get('/', function () {
+//    $title = 'Pa';
+//
+//    return view ('index', [
+//        'title' => $title
+//    ]);
+//});
+
+//Route::get('/post/{id}', function ($id) {
+//    return "post num". $id;
+//});
+//Route::get('/post/admin/example', array('as'=>'admin.home' , function () {
+//    $url = route ('admin.home');
+//    return "this url is". $url;
+//}));
+
+//route::get('/post' , '\App\Http\Controllers\PostController@index');
+
+//use App\Http\Controllers\PostController;
+//route::resource ('post', 'PostController');
