3beac68b1c23daf3aa133f1986d66bdf666107ed
panithace <panthea_mirhomayouni@yahoo.com>
2022-02-05 20:01:09 +0330

    Update create.blade.php

--- project/resources/views/admin/article/create.blade.php
+++ project/resources/views/admin/article/create.blade.php
@@ -1,11 +1,42 @@
-@extends('Layout.project')
+@extends('Layout.Project')
+
 
 @section('content')
+
     <h1> My Project </h1>
- <form action="/admin/article/create",method="post">
+
+    {{--@php--}}
+    {{--dd($errors->all() );--}}
+      {{--@endphp--}}
+      @if($errors->any())
+  <div class="alert alert-danger">
+      <ul>
+        @foreach($errors->all() as $error)
+            <li> {{ $error }} </li>
+        @endforeach
+      </ul>
+</div>
+@endif
+  <form action="/admin/article/create", method="post">
      @csrf
-    <label for="title">title:</label>
-    <input type="text" name="title">
-    <button>Send</button>
-</form>
+     <div class="form-group">
+       <label for="title"> title : </label>
+       <input type="text" name="title" class="form-control">
+    </div>
+    <div class="form-group">
+       <label for="body"> body : </label>
+       <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
+    </div>
+    <button class="btn btn-danger">Send</button>
+
+    {{--<form action"/admin/article/create">--}}
+ {{--<label for="title">title:</label>--}}
+  {{--<<nput type="text" name="title">--}}
+    {{--<button>Send</button>--}}
+    {{--</form>--}}
+
+  {{--<label for="title">title:</label>--}}
+   {{-- <input type="text" name="title">--}}
+    {{--<button>Send</button>--}}
+  </form>
 @endsection
