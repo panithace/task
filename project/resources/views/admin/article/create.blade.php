6741b6fa2fadb40a8759ceda1fa36356b53e4cfe
panithace <panthea_mirhomayouni@yahoo.com>
2022-02-06 20:22:46 +0330

    Update create.blade.php

--- project/resources/views/admin/article/create.blade.php
+++ project/resources/views/admin/article/create.blade.php
@@ -1,42 +1,48 @@
-@extends('Layout.Project')
 
+@extends('Layout.Project')
+{{-- @include('layout.project')--}}
+{{-- <link rel="stylesheet" type="text/css" href="{{asset('css/style.css') }}"> --}}
 
 @section('content')
 
     <h1> My Project </h1>
 
     {{--@php--}}
-    {{--dd($errors->all() );--}}
-      {{--@endphp--}}
+      {{--dd($errors->all());--}}
+        {{-- @endphp--}}
       @if($errors->any())
-  <div class="alert alert-danger">
-      <ul>
-        @foreach($errors->all() as $error)
-            <li> {{ $error }} </li>
-        @endforeach
-      </ul>
-</div>
-@endif
-  <form action="/admin/article/create", method="post">
-     @csrf
-     <div class="form-group">
-       <label for="title"> title : </label>
-       <input type="text" name="title" class="form-control">
-    </div>
+      <div class="alert alert-danger">
+        <ul>
+         @foreach($errors->all() as $error)
+           <li>{{ $error }}</li>
+         @endforeach
+        </ul>
+       </div>
+      @endif
+                    {{--<form action="/admin/article/create">--}}
+                      {{-- @csrf--}}
+                        {{--<div class="form-group">--}}
+                          {{-- <label for="title"> title : </label>--}}
+                            {{-- <input type="text" name="title" class="form-control">--}}
+                              {{-- </div>--}}
+    {{--<div class="form-group">--}}
+    {{--<label for="body"> body : </label>--}}
+       {{--<textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>--}}
+       {{--</div>--}}
+    {{--<button class="btn btn-danger">Send</button>--}}
+
+    <form action="/admin/article/create" method="post">
+    @csrf
     <div class="form-group">
-       <label for="body"> body : </label>
+        <label for="title">title:</label>
+        <input type="text" name="title" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="body"> body : </label>
        <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
-    </div>
+  </div>
     <button class="btn btn-danger">Send</button>
-
-    {{--<form action"/admin/article/create">--}}
- {{--<label for="title">title:</label>--}}
-  {{--<<nput type="text" name="title">--}}
-    {{--<button>Send</button>--}}
-    {{--</form>--}}
-
-  {{--<label for="title">title:</label>--}}
-   {{-- <input type="text" name="title">--}}
-    {{--<button>Send</button>--}}
+{{--input type="text" name="title">--}}
+  {{--<button>Send</button>--}}
   </form>
 @endsection
