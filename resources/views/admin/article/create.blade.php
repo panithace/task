d3b979bc61194598f08d2304995141c9f1c22a23
panithace <panthea_mirhomayouni@yahoo.com>
2022-02-07 21:39:32 +0330

    Update create.blade.php

--- resources/views/admin/article/create.blade.php
+++ resources/views/admin/article/create.blade.php
@@ -1,11 +1,32 @@
-@extends('Layout.project')
+
+@extends('Layout.Project')
+{{-- @include('layout.project')--}}
+{{-- <link rel="stylesheet" type="text/css" href="{{asset('css/style.css') }}"> --}}
 
 @section('content')
+
     <h1> My Project </h1>
- <form action="/admin/article/create",method="post">
-     @csrf
-    <label for="title">title:</label>
-    <input type="text" name="title">
-    <button>Send</button>
-</form>
+
+      @if($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+         @foreach($errors->all() as $error)
+           <li>{{ $error }}</li>
+         @endforeach
+        </ul>
+       </div>
+      @endif
+
+    <form action="/admin/article/create" method="post">
+    @csrf
+    <div class="form-group">
+        <label for="title">title:</label>
+        <input type="text" name="title" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="body"> body : </label>
+       <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
+  </div>
+    <button class="btn btn-danger">Send</button>
+  </form>
 @endsection
