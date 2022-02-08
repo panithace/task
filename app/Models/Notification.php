356076c779fc7ae307e47c7367ac51245cad0e71
panithace <panthea_mirhomayouni@yahoo.com>
2022-02-08 21:30:35 +0330

    9

---
+++ app/Models/Notification.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    //const CREATED_AT = '2022-01-08';
+    //const UPDATED_AT = '2022-01-08';
+    use HasFactory;
+}
