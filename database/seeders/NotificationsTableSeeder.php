9becaf2f02b5f9162aaffaf469a8d021f8807fd9
panithace <panthea_mirhomayouni@yahoo.com>
2022-02-08 21:30:51 +0330

    Create NotificationsTableSeeder.php

---
+++ database/seeders/NotificationsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class NotificationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $faker = \Faker\Factory::create();
+        foreach (range(1,20) as $item) {
+            DB::table('notifications')->insert([
+                'priority' => $faker->numberBetween(47,56),
+                'url' => $faker->text(50),
+                'description' => $faker->paragraph(rand(5,20)),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        } 
+    }
+}
