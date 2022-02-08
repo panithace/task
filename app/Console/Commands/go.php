20f8c87723a9db41fd9303c43fb20f6881230695
panithace <panthea_mirhomayouni@yahoo.com>
2022-02-08 21:30:38 +0330

    Create go.php

---
+++ app/Console/Commands/go.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Notification;
+use Illuminate\Database\Eloquent\Model;
+
+
+//class go extends Command
+//{
+    //protected $signature = 'go';
+      //  public function New($id) {
+        
+    
+        //    $id = DB::table('notifications')
+          //          ->where('priority', '=', 52)
+            //        ->where('constant.List')
+              //      ->delete();
+        //}  
+//}
