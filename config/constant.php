cda7f8ce6c67d9c0adc88fa12f66f86a67f87e87
panithace <panthea_mirhomayouni@yahoo.com>
2022-02-08 21:30:40 +0330

    Create constant.php

---
+++ config/constant.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+
+
+
+/*return [
+'List' => 'DB::table('notifications')
+    ->where('created_at', '=', Carbon::today()->subDays(30)->toDateString())->get()'
+    return ['List'];
+];*/
+?>
