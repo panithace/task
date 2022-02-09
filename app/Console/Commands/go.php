c2d593b8a5ef6d785f709c5deeeffda9bf695e4a
panithace <panthea_mirhomayouni@yahoo.com>
2022-02-09 16:16:10 +0330

    Update go.php

--- app/Console/Commands/go.php
+++ app/Console/Commands/go.php
@@ -4,18 +4,50 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Notification;
-use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
+class Go extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'Go';
 
-//class go extends Command
-//{
-    //protected $signature = 'go';
-      //  public function New($id) {
-        
-    
-        //    $id = DB::table('notifications')
-          //          ->where('priority', '=', 52)
-            //        ->where('constant.List')
-              //      ->delete();
-        //}  
-//}
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $comparison = Carbon::now()->subDays(30);
+        $id = DB::table('notifications')->where('priority','=', 52)->where('created_at', '<=', $comparison)
+       ->delete();
+
+    if ($id) {
+       echo("You're Lucky! Deleted.");
+    } else {
+       echo("Sorry! Couldn't find one."); 
+    }
+}
+    }
